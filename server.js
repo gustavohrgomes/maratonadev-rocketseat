47944a37321e78125c3345c7b905e10d686f16ba
Gustavo Gomes <dev.gustavogomes@gmail.com>
2020-02-23 15:54:48 -0300

    Melhorando a exibiçao de dados no front

--- server.js
+++ server.js
@@ -5,15 +5,52 @@ const server = express()
 // configurar o servidor para apresentar arquivos estáticos
 server.use(express.static('public'))
 
+// habilitar body do formulario
+server.use(express.urlencoded({extended: true}))
+
 //configurando a template engine
 const nunjucks = require("nunjucks")
 nunjucks.configure("./", {
-   express: server
+   express: server,
+   noCache: true
 })
+
+// Lista de Doadores: Vetor ou Array
+const donors = [
+   {
+      name:  "Diego Fernandes",
+      blood: "AB+"
+   },
+   {
+      name:  "Cleiton Souza",
+      blood: "B+"
+   },
+   {
+      name:  "Robson Marques",
+      blood: "A+"
+   },
+   {
+      name:  "Mayk Brito",
+      blood: "O+"
+   }
+]
 
 // configurar a apresenação da página
 server.get("/", function(req, res) {
-   return res.render("index.html")
+   return res.render("index.html", { donors })
+})
+
+server.post("/", function(req, res){
+   const name  = req.body.name
+   const email = req.body.email
+   const blood = req.body.blood
+
+   donors.push({
+      name: name,
+      blood: blood,
+   })
+
+   return res.redirect("/")
 })
 
 // ligar o servidor e permitir o acesso na porta 3000
